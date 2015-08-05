c483fca235ebd1035a6ed5a5b7833491dfcadeda
asharijuang <asharijuang@me.com>
2015-08-05 19:31:50 +0700

    Initial Commit

---
+++ iMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iMusic
+//
+//  Created by Ashari Juang on 8/5/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
