b275bceff4309e4c37186cac4b230c895b1815f1
asharijuang <asharijuang@me.com>
2015-08-06 22:03:10 +0700

    learn iboutlet

--- iMusic/ViewController.h
+++ iMusic/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *logoView;
+
+
 - (IBAction)showPreferences:(id)sender;
 - (IBAction)showList:(id)sender;
 
